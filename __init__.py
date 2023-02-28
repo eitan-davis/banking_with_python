8d99869606164059427dff2a403f93cfc7ef6ecd
eitan-davis <25664700+eitan-davis@users.noreply.github.com>
2023-02-28 09:57:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@
 
 deposit_with_cpi = lambda x, p, cpi, t = 0.25: x * (1 + cpi) * (1 + p * (1- t))
 
-deposit_no_cpi = lambda x, p, t = -0.15: x * (1 + p * (1 - t))
+deposit_no_cpi = lambda x, p, t = 0.15: x * (1 + p * (1 - t))
