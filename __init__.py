ef7124c15dffdb7db7aa67c9f6890c3e029b50a4
eitan-davis <25664700+eitan-davis@users.noreply.github.com>
2023-02-28 09:52:20 +0200

    change m to cpi parameter name

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-deposit_with_cpi = lambda x, p, m, t = 0.25: x * (1 + m) * (1 + p * (1- t))
+deposit_with_cpi = lambda x, p, cpi, t = 0.25: x * (1 + cpi) * (1 + p * (1- t))
 
 deposit_no_cpi = lambda x, p, t = -0.15: x * (1 + p * (1 - t))
